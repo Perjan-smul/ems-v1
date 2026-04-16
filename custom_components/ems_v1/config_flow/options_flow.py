4376a45c2428d7d534d1bcaa8a9e1b53a2bdc8bc
Perjan-smul <134926555+Perjan-smul@users.noreply.github.com>
2026-04-16 14:27:53 +0200

    Create options_flow.py

---
+++ custom_components/ems_v1/config_flow/options_flow.py
@@ -0,0 +1,18 @@
+import voluptuous as vol
+from homeassistant import config_entries
+
+class EMSOptionsFlowHandler(config_entries.OptionsFlow):
+    def __init__(self, config_entry):
+        self.config_entry = config_entry
+
+    async def async_step_init(self, user_input=None):
+
+        if user_input is not None:
+            return self.async_create_entry(title="", data=user_input)
+
+        return self.async_show_form(
+            step_id="init",
+            data_schema=vol.Schema({
+                vol.Optional("enable_simulation", default=True): bool,
+            }),
+        )
